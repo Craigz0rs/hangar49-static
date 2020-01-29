6b9fc3fdc381c3372ca237afd60f1996fe45788a
Craigz0rs <cdarcy@live.ca>
2020-01-29 00:01:10 -0800

    wip(testing)acf fields

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,16 +3,18 @@
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
+<!-- 
+    <p>{{$page.fields.acf.siteIntro}}</p> -->
+    <!-- <div v-for="field in stuff">
+      <h1> {{ field.headline }} </h1>
+      <h2> {{ field.introText }} </h2>
+    </div> -->
 
-    <p>{{$page.fields.acf.siteIntro}}</p>
-    <!-- <div v-for="field in $page.fields.acf.siteIntro" :key="field.headline">
-      <h1 {{ $page
-       }} </h1>
-      </div> -->
+      <h3> {{headline}} </h3>
+      <h4> {{introText}} </h4>
+      <!-- <p>{{stuff}}</p> -->
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+
 
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
@@ -39,8 +41,35 @@ query {
 export default {
   metaInfo: {
     title: 'Hello, world!'
+  },
+  data() {
+    return {
+      stuff: null,
+      headline: null,
+      introText: null
+    }
+  },
+  computed: {
+    acf() {
+      this.headline = "blah"
+      for (let field in this.stuff) {
+         return this.headline = field.headline
+        // this.introText = field.introText
+      }
+    }
+  },
+  created() {
+    // console.log(this.$page.fields.acf.siteIntro);
+    this.stuff = this.$page.fields.acf.siteIntro;
+    // this.acf
+      for(let field in this.$page.fields.acf.siteIntro) {
+        console.log(typeof(field));
+        this.headline = this.$page.fields.acf.siteIntro[0].headline;
+        this.introText = this.$page.fields.acf.siteIntro[0].introText;
+      }
+    }
   }
-}
+
 </script>
 
 <style>
